1e205a1b095bbffe26403fef77c241b52feb17d7
Ashhar-24 <mdashhar01@gmail.com>
2024-02-15 20:05:08 +0530

    Week 0

---
+++ Week1/a.cpp
@@ -0,0 +1,16 @@
+/*
+    To compile this file: g++ a.cpp -o temp, ie changes "a.cpp" file into binary file of the name "temp"
+    To run this binary file, use "./temp" on the terminal to get output, if there's no compilation error, the output is displayed
+    else not.
+
+    Whereas in JS, the code is not compiled and then executed, it is directly executed and therefore when errors are present
+    it is partially executed upto the line of code which is correct.
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hello, Ashhar here!"<<'\n';
+    int a=454;
+    cout<<a<<'\n';
+}
---
+++ Week1/first_class.js
@@ -0,0 +1,18 @@
+// console.log("Hey, Ashhar here!")
+// console.log("blablabla")
+// let a=15;
+// a=3;
+
+// Declaring the variable
+
+var a=4;
+a="Hello"
+console.log(a)
+
+let c="Hello"
+c=23
+console.log(c)
+
+const s="Hey"
+// s=23
+console.log(s)
